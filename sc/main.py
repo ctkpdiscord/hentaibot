ba3a0fcfeac11f6d00f604fda1d34272051659e2
zearakun <91083604+zearakun@users.noreply.github.com>
2022-11-21 21:21:18 +0900

    Update main.py

--- sc/main.py
+++ sc/main.py
@@ -5,34 +5,34 @@ import requests
 
 token = ""
 prefix = "h!"
+commandlist = ["hass", "hmidriff", "pgif", "4k", "hentai", "hneko", "neko", "hkitsune", "kemonomimi", "anal", "hanal", "gonewild", "kanna", "ass", "pussy", "thigh", "hthigh",  "paizuri", "tentacle", "boobs", "hboobs", "yaoi"]
+
 bot = commands.Bot(command_prefix="h!",intents=intents)
 
-@bot.event
-async def on_connect():
-  print("起動完了")
-command = ["hass", "hmidriff", "pgif", "4k", "hentai", "hneko", "neko", "hkitsune", "kemonomimi", "anal", "hanal", "gonewild", "kanna", "ass", "pussy", "thigh", "hthigh",  "paizuri", "tentacle", "boobs", "hboobs", "yaoi"]
-@bot.listen("on_message")
-async def cmdcommands(message):
-  if message.content[:3] == "h!":
-    if message.content[3:]:
-  if message.content == prefix+"hentai":
-    image = requests.get("https://nekobot.xyz/api/image", headers={
+async def genimage(ctx,type)
+  image = requests.get("https://nekobot.xyz/api/image", headers={
                         "User-Agent": "NekoBotAPI-py/1.0",
                         "Authorization": ""
-                        },params={"type": "hentai"}).json()["message"]
-  elif message.content == prefix+"anal":
-    image = requests.get("https://nekobot.xyz/api/image", headers={
-                        "User-Agent": "NekoBotAPI-py/1.0",
-                        "Authorization": ""
-                        },params={"type": "anal"}).json()["message"]
-  elif message.content == prefix+"anal":
-    image = requests.get("https://nekobot.xyz/api/image", headers={
-                        "User-Agent": "NekoBotAPI-py/1.0",
-                        "Authorization": ""
-                        },params={"type": "anal"}).json()["message"]
+                        },params={"type": type}).json()["message"]
   embed = discord.Embed(title="Hentai Image")
   embed.set_thumbnail(url=image)
   await ctx.send(embed=embed)
 
+  
+@bot.event
+async def on_connect():
+  print("起動完了")
+
+@bot.listen("on_message")
+async def cmdcommands(message):
+  if message.content[:3] == "h!":
+    if message.content == "h!help":
+      embed = discord.Embed(title="HentaiHelp",description="\n".join("h!"+i for i in command))
+      await ctx.send(embed=embed)
+      return
+    for command in commandlist:
+      if message.content[3:] == command:
+        await genimage(ctx,command)
+        return
 
 bot.run(token)
