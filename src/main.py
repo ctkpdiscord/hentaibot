a6a8e8e51f439303598f2c832d6755b3d8b04bbd
zearakun <91083604+zearakun@users.noreply.github.com>
2022-11-21 21:35:03 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-import discord
+import discord,requests,json
 from discord import commands
-import requests
+jsondata = json.load(open("./config/config.json","r"))
 
 
-token = ""
-prefix = "h!"
+token = jsondata["TOKEN"]
+prefix = jsondata["PREFIX"]
 commandlist = ["hass", "hmidriff", "pgif", "4k", "hentai", "hneko", "neko", "hkitsune", "kemonomimi", "anal", "hanal", "gonewild", "kanna", "ass", "pussy", "thigh", "hthigh",  "paizuri", "tentacle", "boobs", "hboobs", "yaoi"]
 
-bot = commands.Bot(command_prefix="h!",intents=intents)
+bot = commands.Bot(command_prefix=prefix,intents=intents)
 
 async def genimage(ctx,type)
   image = requests.get("https://nekobot.xyz/api/image", headers={
@@ -25,9 +25,9 @@ async def on_connect():
 
 @bot.listen("on_message")
 async def cmdcommands(message):
-  if message.content[:3] == "h!":
-    if message.content == "h!help":
-      embed = discord.Embed(title="HentaiHelp",description="\n".join("h!"+i for i in command))
+  if message.content[:3] ==prefix:
+    if message.content == prefix+"help":
+      embed = discord.Embed(title="HentaiHelp",description="\n".join(prefix+i for i in command))
       await ctx.send(embed=embed)
       return
     for command in commandlist:
